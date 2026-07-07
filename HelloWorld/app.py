b34eca42e69b35eb73a67f379eef2e27248744e8
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-07-07 11:46:53 +0300

    Added hello world project, getting to know Python

---
+++ HelloWorld/app.py
@@ -0,0 +1,17 @@
+age = 23
+age = 30
+print(age)
+
+#concatenation
+name = input("Enter your name: ")
+print ("Hello " + name)
+
+#type conversion
+first = float(input("First: "))
+second = float(input("Second: "))
+sum = first + second
+print("Sum: " + str(sum))
+
+#string
+course = 'Python for beginners' #the string here - bsc an object
+
