94237afb8853b347a83b90b8d4831b364ce19e86
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-07-15 17:30:56 +0300

    added reverse_string

---
+++ Python_Basics_Exercise/Reverse_String.py
@@ -0,0 +1,10 @@
+def reverse_string(text):
+    return text[::-1]
+
+    #result = ""
+    #for char in text:
+        #result = char + result
+    #return result
+
+word = input("Enter a word: ")
+print(reverse_string(word))
