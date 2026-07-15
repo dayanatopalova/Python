8c8c608d32caffb65c0b6076836f370d00c8809d
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-07-15 16:22:22 +0300

    added finding the max number

---
+++ Python_Basics_Exercise/Max_Number.py
@@ -0,0 +1,18 @@
+numbers = []
+
+while True:
+    value = input("Enter a number or 'stop' - command: ")
+
+
+    if value.lower() == "stop":
+        break
+
+    numbers.append(int(value))
+
+largest = numbers[0]
+
+for number in numbers:
+    if number > largest:
+        largest = number
+
+print(f"The number {largest} is the largest one.")
